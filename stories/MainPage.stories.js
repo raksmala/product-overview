9ad8bc512ff9b39889b1f0e97aa09cce48fe0aba
raksmala <raksmala@gmail.com>
2024-04-22 16:32:07 +0700

    fix: change viewport

--- stories/MainPage.stories.js
+++ stories/MainPage.stories.js
@@ -1,4 +1,4 @@
-import "./mainpage.css";
+import './mainpage.css';
 import logo from "../img/logo.png";
 import black from "../img/black.png";
 import green from "../img/green.png";
@@ -8,16 +8,8 @@ import pink from "../img/pink.png";
 export default {
   title: "Main Page",
   parameters: {
-    viewport: {
-      defaultViewport: "md",
-    },
-    chromatic: {
-      modes: {
-        lg: allModes["lg"],
-        xl: allModes["xl"],
-      },
-    },
-  },
+    chromatic: { viewports: [1536] }
+  }
 };
 
 export const Default = () => {
